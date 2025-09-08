8925d356cf0ad1b53285517b709b14efeb1de809
Yehezkiel Stephanus Austin <yehezkiel.stephanus@gmail.com>
2025-09-08 21:47:17 +0700

    feat(chore): cleaning code

--- complaint-reporting-api/complaint-reporting-api/src/main/java/com/example/complaint_reporting_api/controller/ComplaintController.java
+++ complaint-reporting-api/complaint-reporting-api/src/main/java/com/example/complaint_reporting_api/controller/ComplaintController.java
@@ -3,15 +3,15 @@ package com.example.complaint_reporting_api.controller;
 import com.example.complaint_reporting_api.dto.complaint.ChangeComplainStatusRequest;
 import com.example.complaint_reporting_api.dto.complaint.CreateComplainRequest;
 import com.example.complaint_reporting_api.entity.ComplaintEntity;
-import com.example.complaint_reporting_api.entity.Status;
 import com.example.complaint_reporting_api.service.ComplaintService;
+
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import java.util.List;
 
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/complaints")
@@ -20,21 +20,16 @@ public class ComplaintController {
     @Autowired
     ComplaintService complaintService;
 
-    @GetMapping
-    public ResponseEntity<List<ComplaintEntity>> getAllComplaint(@RequestParam(required = false) String status){
-        return complaintService.getAllComplaint(status);
-    }
-
     // create complaint
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<ComplaintEntity> createComplaint(@Valid @RequestBody CreateComplainRequest req){
         return complaintService.createComplaints(req);
     }
 
-
-    @PutMapping("/{id}")
-    public ResponseEntity<ComplaintEntity> updateComplaintStatus(@PathVariable Long id, @RequestBody ChangeComplainStatusRequest status) {
-        return complaintService.updateStatus(id, status);
+    // get all complaint
+    @GetMapping
+    public ResponseEntity<List<ComplaintEntity>> getAllComplaint(@RequestParam(required = false) String status){
+        return complaintService.getAllComplaint(status);
     }
 
     // get complaint detail
@@ -42,6 +37,12 @@ public class ComplaintController {
     public ResponseEntity<ComplaintEntity> getComplaintDetail(@PathVariable Long id){
         return complaintService.getComplaintDetail(id);
 
+    }
+
+    // update complaint status
+    @PutMapping("/{id}")
+    public ResponseEntity<ComplaintEntity> updateComplaintStatus(@PathVariable Long id, @RequestBody ChangeComplainStatusRequest status) {
+        return complaintService.updateStatus(id, status);
     }
 
 }
--- complaint-reporting-api/complaint-reporting-api/src/main/java/com/example/complaint_reporting_api/service/ComplaintService.java
+++ complaint-reporting-api/complaint-reporting-api/src/main/java/com/example/complaint_reporting_api/service/ComplaintService.java
@@ -5,14 +5,13 @@ import com.example.complaint_reporting_api.dto.complaint.CreateComplainRequest;
 import com.example.complaint_reporting_api.entity.ComplaintEntity;
 import com.example.complaint_reporting_api.entity.Status;
 import com.example.complaint_reporting_api.repository.ComplaintRepo;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-
 import java.time.LocalDateTime;
 import java.util.Optional;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,24 +21,6 @@ public class ComplaintService {
 
     @Autowired
     private ComplaintRepo complaintRepo;
-
-    public ResponseEntity<List<ComplaintEntity>> getAllComplaint(String status){
-        List<ComplaintEntity> listData = complaintRepo.findAll();
-        List<ComplaintEntity> filteredData = new ArrayList<>();
-        if(status.isEmpty()) return ResponseEntity.ok(listData);
-
-        try{
-            Status stats = Status.valueOf(status.toUpperCase());
-            for(ComplaintEntity c : listData){
-                if (c.getStatus().equals(stats)) filteredData.add(c);
-            }
-
-            return ResponseEntity.ok(filteredData);
-        } catch (IllegalArgumentException e){
-            return ResponseEntity.noContent().build();
-        }
-
-    }
 
     public ResponseEntity<ComplaintEntity> createComplaints(CreateComplainRequest req){
         try{
@@ -55,13 +36,33 @@ public class ComplaintService {
         }
     }
 
+    public ResponseEntity<List<ComplaintEntity>> getAllComplaint(String status){
+        List<ComplaintEntity> listData = complaintRepo.findAll();
+        List<ComplaintEntity> filteredData = new ArrayList<>();
+        if(status.isEmpty()) return ResponseEntity.ok(listData);
+        try{
+            Status stats = Status.valueOf(status.toUpperCase());
+            for(ComplaintEntity c : listData){
+                if (c.getStatus().equals(stats)) filteredData.add(c);
+            }
+
+            return ResponseEntity.ok(filteredData);
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.noContent().build();
+        }
+    }
+
+    public ResponseEntity<ComplaintEntity> getComplaintDetail(Long id){
+        ComplaintEntity tempComplaint = complaintRepo.findById(id).orElse(null);
+        if (tempComplaint == null) return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(tempComplaint);
+    }
 
     public ResponseEntity<ComplaintEntity> updateStatus(Long id, ChangeComplainStatusRequest status) {
         Optional<ComplaintEntity> complaintOpt = complaintRepo.findById(id);
         if (complaintOpt.isEmpty()) {
             return ResponseEntity.notFound().build();
         }
-
         try{
             ComplaintEntity tempComplaint = complaintOpt.get();
             Status stats = Status.valueOf(status.getStatus().toUpperCase());
@@ -71,15 +72,7 @@ public class ComplaintService {
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().build();
         }
-
-
     }
 
-    public ResponseEntity<ComplaintEntity> getComplaintDetail(Long id){
-        ComplaintEntity tempComplaint = complaintRepo.findById(id).orElse(null);
-        if (tempComplaint == null) return ResponseEntity.notFound().build();
 
-
-        return ResponseEntity.ok(tempComplaint);
-    }
 }
