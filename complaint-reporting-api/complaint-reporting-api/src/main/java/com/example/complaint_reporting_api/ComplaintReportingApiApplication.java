663228f291bea5dd383fbd346ce6b24db4ee30ae
Yehezkiel Stephanus Austin <yehezkiel.stephanus@gmail.com>
2025-09-08 18:54:26 +0700

    feat(chore): git module pom etc

---
+++ complaint-reporting-api/complaint-reporting-api/src/main/java/com/example/complaint_reporting_api/ComplaintReportingApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.complaint_reporting_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintReportingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintReportingApiApplication.class, args);
+	}
+
+}
---
+++ complaint-reporting-api/complaint-reporting-api/src/test/java/com/example/complaint_reporting_api/ComplaintReportingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.complaint_reporting_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplaintReportingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
